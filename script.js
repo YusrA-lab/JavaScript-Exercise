680778c13c51e4dde7aee5155fc02cd3c44de01c
Yusra <yusraabrahams18@gmail.com>
2025-06-02 13:57:21 +0200

    created index.html

---
+++ script.js
@@ -0,0 +1,18 @@
+// TODO: Create a function called greet that takes a name parameter
+function greet(name) {
+    console.log(`Hello, ${name}!`); 
+}
+// TODO: Make the greet function return "Hello, [name]!"
+greet("Yusra");
+// TODO: Create a function called add that takes two parameters
+function add(a,b){
+    console.log(`The sum of ${a} and ${b} is: ${a + b}`);
+}
+// TODO: Make the add function return the sum of both parameters
+add(5,10);
+// TODO: Call both functions and store their results in variables
+let greeting = greet("Yusra");
+let sumResult = add(5, 10);
+// TODO: Print the results to the console
+console.log(greeting);
+console.log(sumResult);
