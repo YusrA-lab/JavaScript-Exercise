1d3f3829158f38e7f374785ab86035048fb94c62
Yusra <yusraabrahams18@gmail.com>
2025-06-02 13:46:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ let firstName = "Yusra";
+ let lastName = "Abrahams";
+ let age = 23;
+ let fullName = firstName + lastName;
+ let greeting = "Hello, my name is " + fullName + "and i am " + age + "years old.";
+ console.log(greeting);
+ age = 35;
+ console.log("JavaScript Exercise 01: DONE");
