6056b435dfad265e61f8f357eec45d8ede9dfe9a
Yusra <yusraabrahams18@gmail.com>
2025-06-02 13:51:07 +0200

    created index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+ let number1 = 21;
+let number2 = 17;
+console.log(typeof String (number1));
+let sum = number1 + number2;
+let product = number1 * number2;
+console.log("The sum of " + number1 + " and " + number2 + " is " + sum);
+console.log("The product of " + number1 + "and " + number2 + " is " + product);
+        
